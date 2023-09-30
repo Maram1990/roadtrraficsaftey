a050eeb0f63b1a95a7ed9a29bb9b51b998c6932d
Maram90 <maram.as.mot@gmail.com>
2023-09-30 22:00:56 +0300

    third

--- resources/views/layouts/review.blade.php
+++ resources/views/layouts/review.blade.php
@@ -38,9 +38,8 @@
 </head>
 
 <body>
-
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
 
       <h1 class="logo"><img src="c:\Users\mram\Downloads\لوغو وزارة النقل  للخلفية.png" alt="" class="img-fluid"></a><a href="index.html">السلامة المرورية</a></h1>
@@ -72,70 +71,11 @@
 
     </div>
   </header><!-- End Header -->
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
 
 
-<body>
+  @yield('content')
 
-</body>
-
-<!-- login -->
-
-    <div class="container  "  style="background-color: #f6f7ff;">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>Log In</h1>
-               <p>You don't have a password? Then please <a class="white" href="sign-up.html">Sign Up</a></p>
-                <!-- Sign Up Form -->
-                <div class="form-container">
-                    <form id="logInForm" data-toggle="validator" data-focus="false">
-                        <div class="form-group">
-                            <input type="email" class="form-control-input" id="lemail" required>
-                            <label class="label-control" for="lemail">Email</label>
-                            <div class="help-block with-errors"></div>
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control-input" id="lpassword" required>
-                            <label class="label-control" for="lpassword">Password</label>
-                            <div class="help-block with-errors"></div>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="form-control-submit-button">LOG IN</button>
-                        </div>
-                        <div class="form-message">
-                            <div id="lmsgSubmit" class="h3 text-center hidden"></div>
-                        </div>
-                    </form>
-                </div> <!-- end of form container -->
-                <!-- end of sign up form -->
-
-            </div> <!-- end of col -->
-        </div> <!-- end of row -->
-    </div> <!-- end of container -->
-
-<!-- end of login -->
-<br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-
-</body>
-
-<!-- ======= Footer ======= -->
-<footer id="footer">
+  <footer id="footer">
 
 
 
@@ -163,13 +103,13 @@
     </div>
   </footer><!-- End Footer -->
 
+</body>
+</html>
 
-<!-- Scripts -->
-<script src="js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
-<script src="js/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
-<script src="js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
-<script src="js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
-<script src="js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
-<script src="js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
-<script src="js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
-<script src="js/scripts.js"></script> <!-- Custom scripts -->
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('signs');
 });
 
 Route::get('/maram', function () {
@@ -56,7 +56,7 @@ route::get('contact',function(){
     $name=request("name");
     return $name;
 });
- 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
